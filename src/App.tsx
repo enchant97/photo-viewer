d2709eaf6bd394602e977b128b5cd9ad43ebc9ab
Leo Spratt <enchant97@users.noreply.github.com>
2022-08-28 17:23:40 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { createEffect, createSignal, For } from "solid-js";
+import { open } from '@tauri-apps/api/dialog';
+import { appDir } from '@tauri-apps/api/path';
+import { readDir, readBinaryFile } from '@tauri-apps/api/fs';
+
+import "./App.css";
+
+function Img(props: any) {
+  let [srcUrl, setSrcUrl] = createSignal("");
+
+  createEffect(async () => {
+    let contents = await readBinaryFile(props.path);
+    let blob = new Blob([contents], { type: "image/jpeg" });
+    setSrcUrl(URL.createObjectURL(blob));
+  });
+
+  return <img src={srcUrl()} alt="" />;
+}
+
+function App() {
+  let [dir, setDir] = createSignal("");
+  let [files, setFiles] = createSignal<string[]>([]);
+  let [dirs, setDirs] = createSignal<string[]>([]);
+
+  async function handleDirPickClick() {
+    let selected = await open({
+      directory: true,
+      multiple: false,
+      defaultPath: await appDir(),
+    });
+    if (!Array.isArray(selected) && selected !== null) {
+      setDir(selected);
+      await refreshGrid();
+    }
+  }
+
+  async function refreshGrid() {
+    let root_dir = dir();
+    if (root_dir) {
+      let entries = await readDir(root_dir, { recursive: false });
+      var new_files = [];
+      var new_dirs = [];
+      for (let entry of entries) {
+        if (entry.children === undefined) {
+          if ((entry.path.toLowerCase()).endsWith(".jpg")){
+            new_files.push(entry.path);
+          }
+        } else {
+          new_dirs.push(entry.path)
+        }
+      }
+      setFiles(new_files);
+      setDirs(new_dirs);
+    }
+  }
+
+  return (
+    <>
+      <h1>Photo Viewer</h1>
+      <button onClick={() => handleDirPickClick()}>Pick Folder</button>
+      <p>{dir}</p>
+      <h2>Directories</h2>
+      <ul>
+        <For each={dirs()}>
+          {(path) => <li>{path}</li>}
+        </For>
+      </ul>
+      <h2>Files</h2>
+      <ul>
+        <For each={files()}>
+          {(path) => <li><Img path={path}></Img></li>}
+        </For>
+      </ul>
+    </>
+  );
+}
+
+export default App;
