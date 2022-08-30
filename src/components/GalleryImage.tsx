6bed51247782186ec3e6ea93ac16159c4619c532
Leo Spratt <enchant97@users.noreply.github.com>
2022-08-30 15:29:19 +0100

    load&unload images off-screen

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -1,5 +1,30 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 import { createResourceURI } from "../core/helpers";
+
+const ObservedImage: Component<any> = (props) => {
+  let imageRef: HTMLImageElement | undefined;
+  let [isIntersecting, setIntersecting] = createSignal(false);
+  let [srcUrl, setSrcUrl] = createSignal("");
+
+  createEffect(() => {
+    let observer = new IntersectionObserver(([entry]) => { setIntersecting(entry.isIntersecting) });
+    if (imageRef) { observer.observe(imageRef) }
+  });
+
+  createEffect(() => {
+    if (isIntersecting()) { setSrcUrl(props.src) }
+    else { setSrcUrl("") }
+  });
+
+  return (
+    <img
+      ref={imageRef}
+      src={srcUrl()}
+      alt={props.alt} style={props.style}
+      onclick={props.onclick}
+    />
+  );
+}
 
 export type GalleryImageProps = {
   imagePath: string
@@ -11,12 +36,11 @@ const GalleryImage: Component<GalleryImageProps> = (props) => {
   const handleOnClick = () => { props.onClick?.(props.imagePath) }
 
   return (
-    <img
+    <ObservedImage
       src={createResourceURI(props.imagePath)}
       alt="" style={"width: " + props.width}
       onclick={handleOnClick}
-    />
-  );
+    />);
 }
 
 export default GalleryImage;
